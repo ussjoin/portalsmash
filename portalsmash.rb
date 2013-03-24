72fa6884e092adb0a9e701cb21b9474c1ff11717
Brendan O'Connor <brendan@maliceafterthought.com>
2013-03-24 12:09:05 -0700

    Known networks now first in the wpa_supplicant configuration file. Returns errors correctly when iwlist doesn't work (e.g., when you specify the wrong device).

--- portalsmash.rb
+++ portalsmash.rb
@@ -65,10 +65,18 @@ class PortalSmasher
   def scan
     puts "Scanning"
     
+    encnets = []
+    unencnets = []
+    
     File.open(CONFPATH, "w") do |f|
       f.puts "ctrl_interface=DIR=/var/run/wpa_supplicant"
     
       networklist = `iwlist #{@device} scan`;
+      
+      if ($?.exitstatus != 0)
+        return false #iwlist didn't work right.
+      end
+      
       networks = networklist.split(/Cell \d{2}/); #This will give us cell 1 in @networks[1], as [0] will hold junk
       networks.delete_at(0)
     
@@ -84,41 +92,63 @@ class PortalSmasher
         #if we haven't done this network before (to prevent trying to connect to 80 different instances of
         #the same WiFi network)7888888
         if (!usednetworks[ssid] and ((enc == "off") or (@knownnetworks[ssid])))
-          f.puts "network={"
-          f.puts "ssid=\"#{ssid}\""
-          f.puts "scan_ssid=1"
+          str = ""
+          str += "network={\n"
+          str += "ssid=\"#{ssid}\"\n"
+          str += "scan_ssid=1\n"
           if (enc == "on")
             # This is just a brutal hack. I can be a lot more precise-- specifying CCMP and the like-- but it doesn't matter, weirdly.
             
             if net =~ /WPA/
               if (@knownnetworks[ssid]['key']) #Then it's WPA-PSK
-                f.puts "key_mgmt=WPA-PSK"
-                f.puts "psk=\"#{@knownnetworks[ssid]['key']}\""
+                str += "key_mgmt=WPA-PSK\n"
+                str += "psk=\"#{@knownnetworks[ssid]['key']}\"\n"
               else #Then it's WPAE
-                f.puts "key_mgmt=WPA-EAP"
-                f.puts "identity=\"#{@knownnetworks[ssid]['username']}\""
-                f.puts "password=\"#{@knownnetworks[ssid]['password']}\""
+                str += "key_mgmt=WPA-EAP\n"
+                str += "identity=\"#{@knownnetworks[ssid]['username']}\"\n"
+                str += "password=\"#{@knownnetworks[ssid]['password']}\"\n"
               end
             else #WEP
-              f.puts "key_mgmt=NONE"
-              f.puts "wep_tx_keyidx=0"
-              f.puts "wep_key0=\"#{@knownnetworks[ssid]['key']}\""
+              str += "key_mgmt=NONE\n"
+              str += "wep_tx_keyidx=0\n"
+              str += "wep_key0=\"#{@knownnetworks[ssid]['key']}\"\n"
             end
             
           else
-            f.puts "key_mgmt=NONE"
+            str += "key_mgmt=NONE\n"
           end
-          f.puts "}"
-          f.puts ""
+          str += "}\n"
+          
+          usednetworks[ssid] = 1
+          
+          if @knownnetworks[ssid]
+            encnets.push(str)
+          else
+            unencnets.push(str)
+          end
+          
         end
-        
-        usednetworks[ssid] = 1
-        
+      end
+
+      puts "Encnets: #{encnets.size} Unencnets: #{unencnets.size}"
+
+      encnets.each do |s|
+        f.puts s
+      end
+      
+      unencnets.each do |s|
+        f.puts s
       end
 
     end
     
-    exit(0)
+    if (encnets.size + unencnets.size == 0)
+      return false
+    end
+    
+    true
+    
+    #exit(0)
   end
   
   def attach
@@ -170,11 +200,13 @@ class PortalSmasher
       puts "State: #{@state}"
       case @state
       when :start
-        scan
+        @scan_success = scan
         if @scan_success
           @state = :list
         else
           @state = :start
+          puts "Scan failed using #{@device}."
+          sleep(2)
         end
       when :list
         attach
